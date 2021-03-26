1e7f28ace7df126bd2bdca17863b9b2517a9f97a
Your Name <baenztim@students.zhaw.ch>
2021-03-26 15:26:13 +0100

    Aufgabe_Woche_5_26032021

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/Aufgabe_Woche_5_Test.java
@@ -0,0 +1,18 @@
+package ch.zhaw.iwi.devops.fizzbuzz;	
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class Aufgabe_Woche_5_Test { 
+
+
+    @Test
+    public void schnappszahlTest(){
+        Aufgabe_Woche_5 aw5 = new Aufgabe_Woche_5();
+        Assert.assertEquals("Schnappszahl", aw5.schnappszahl(33));
+
+
+    }
+
+}
